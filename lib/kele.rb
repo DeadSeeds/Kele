af1261b8d7e57fa0a8b0c0571fb3fa709776ba37
Joshua Brown <xxdeadseeds@gmail.com>
2018-05-04 15:53:32 -0700

    checkpoint 3 complete

--- lib/kele.rb
+++ lib/kele.rb
@@ -1,4 +1,5 @@
 require 'httparty'
+require 'json'
 
 class Kele
   include HTTParty
@@ -10,12 +11,18 @@ class Kele
 
     response = self.class.post("#{@bloc_session}/sessions", body: @auth)
     auth_token = response["auth_token"]
+    @auth_token = auth_token
 
     if auth_token.nil?
       p "Something went wrong, please try again"
     else
       p "Authorization received. Auth_token is: #{auth_token}"
     end
+  end
+
+  def get_me
+    response = self.class.get("#{@bloc_session}/users/me", headers: { "authorization" => @auth_token })
+    JSON.parse("#{response}")
   end
 
 end
