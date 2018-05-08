9394cef7f4c7a3573ae01ccd5ef824ab9f2a523d
Joshua Brown <xxdeadseeds@gmail.com>
2018-05-08 09:54:56 -0700

    remove unused variables

--- lib/messages.rb
+++ lib/messages.rb
@@ -2,17 +2,16 @@ module Messages
   def get_messages(page = nil)
     if page.nil?
       response = self.class.get("#{@bloc_session}/message_threads", headers: { "authorization" => @auth_token })
-      message_page = JSON.parse("#{response}")
+      JSON.parse("#{response}")
     else
       page_number = { page: page }
       response = self.class.get("#{@bloc_session}/message_threads", body: page_number, headers: { "authorization" => @auth_token })
-      message_page = JSON.parse("#{response}")
+      JSON.parse("#{response}")
     end
   end
 
-  def create_message(sender:, recipient_id:, stripped_text:, token: nil, subject: nil)
+  def create_message(sender, recipient_id, stripped_text, token: nil, subject: nil)
     message = { "sender" => "#{sender}", "recipient_id" => recipient_id, "token" => "#{token}", "subject" => "#{subject}", "stripped-text" => "#{stripped_text}" }
-    binding.pry
-    response = self.class.post("#{@bloc_session}/messages", body: message, headers: { "authorization" => @auth_token})
+    self.class.post("#{@bloc_session}/messages", body: message, headers: { "authorization" => @auth_token})
   end
 end
--- lib/roadmap.rb
+++ lib/roadmap.rb
@@ -2,12 +2,12 @@ module Roadmap
 
   def get_roadmap(chain_id)
     response = self.class.get("#{@bloc_session}/roadmaps/"+ "#{chain_id}", headers: { "authorization" => @auth_token })
-    roadmap = JSON.parse("#{response}")
+    JSON.parse("#{response}")
   end
 
   def get_checkpoint(checkpoint_id)
     response = self.class.get("#{@bloc_session}/checkpoints/" + "#{checkpoint_id}", headers: { "authorization" => @auth_token })
-    checkpoint = JSON.parse("#{response}")
+    JSON.parse("#{response}")
   end
 
 end
